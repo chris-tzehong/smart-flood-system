fa09403105f19550a67e62be0acf5e494a3c362c
Aw Eumund <vermithrex@users.noreply.github.com>
2019-12-14 13:42:23 +0800

    Update ThreadsFragment

--- app/src/main/java/com/example/smartfloodsystem/ThreadsFragment.java
+++ app/src/main/java/com/example/smartfloodsystem/ThreadsFragment.java
@@ -1,27 +1,38 @@
 package com.example.smartfloodsystem;
 
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ThreadsFragment extends Fragment {
-
-
-    public ThreadsFragment() {
-        // Required empty public constructor
-    }
-
+    private TextView mThreadTitle;
+    private ImageView mThreadImage;
+    private TextView mThreadContent;
+    private Button mAddComments;
+    private RecyclerView mThreadComments;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_threads, container, false);
+        View v = inflater.inflate(R.layout.fragment_threads, container, false);
+
+        mThreadTitle = (TextView) v.findViewById(R.id.thread_title);
+        mThreadImage = (ImageView) v.findViewById(R.id.thread_imageView);
+        mThreadContent = (TextView) v.findViewById(R.id.thread_content);
+        mAddComments = (Button) v.findViewById(R.id.button_addComments);
+        mThreadComments = (RecyclerView) v.findViewById(R.id.thread_comments_recycler_view);
+
+        return v;
     }
 
 }
